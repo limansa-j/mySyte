70282a343d17d9fa5e6b177a663d35589e52240d
limansa-j <limansa@yandex.ru>
2019-03-06 20:37:15 +0300

    form modofoed

--- form/index.php
+++ form/index.php
@@ -5,25 +5,43 @@
 		<meta charset="UTF-8">
 	</head>
 		<body>
-			<form method="get" action="get.php">
-					Имя <input type="text" name="name" placeholder="Введите ваше имя"/><br/>
-					Сообщение <input type="text" name="message" placeholder="Введите ваше сообщение"/><br/>
-					<input type="submit"/>
-			</form> 
-
+<!-- 			<form method="get" action="get.php">
+		Имя <input type="text" name="name" placeholder="Введите ваше имя"/><br/>
+		Сообщение <input type="text" name="message" placeholder="Введите ваше сообщение"/><br/>
+		<input type="submit"/>
+</form>  -->
+			<h1>Анкета</h1>
 			<form method="post" action="post.php">
 
-					Имя <input type="text" name="name" placeholder="Введите ваше имя" /><br/>
-					Сообщение <textarea name="message" placeholder="Введите ваше сообщение"></textarea><br/>
-					<input type="checkbox" name="flag">
-					<input type="checkbox" name="flag">
-					<input type="checkbox" name="flag">
-					<input type="checkbox" name="flag">
+					Имя <input type="text" name="name" placeholder="Введите имя" /><br/>
+					Фамилия <input type="text" name="surname" placeholder="Введите фамилию" /><br/>
+					Введите дату рождения <input type="text" name="birthdate" size="2" />
+					<select name="month">
+						<option>Января</option>
+						<option>Февраля</option>
+						<option>Марта</option>
+						<option>Апреля</option>
+						<option>Мая</option>
+						<option>Июня</option>
+						<option>Июля</option>
+						<option>Августа</option>
+						<option>Сентября</option>
+						<option>Октября</option>
+						<option>Ноября</option>
+						<option>Декабря</option>
+					</select>
+					<input type="text" name="year" size="2" value="20"/>г.<br/>
+					Номер телефона
+					<input type="tel" name="tel" placeholder="+7(xxx)xxx-xx-xx"/><br/>
+					Пол:<br/>
+					мужской
+					<input type="radio" name="radio" value="m">
+					женский
+					<input type="radio" name="radio" value="w"><br/>
+					Ваши интересы
+					<textarea name="message" placeholder="Коротко о себе"></textarea><br/>
+					Я даю согласие на обработку персональных данных
 					<input type="checkbox" name="flag"><br/>
-					<input type="radio" name="radio">
-					<input type="radio" name="radio">
-					<input type="radio" name="radio">
-					<input type="radio" name="radio"><br/>
 					<input type="hidden" id="date" value="04.03.2019">
 					<input type="submit" onclick="alert(document.getElementById('date').value)" />
 			</form>
--- form/post.php
+++ form/post.php
@@ -1,7 +1,18 @@
 <? header('Content-Type: text/html; charset=utf-8'); 
 	$name = $_POST['name'];
+	$surname = $_POST['surname'];
+	$date = $_POST['birthdate'];
+	$month = $_POST['month'];
+	$year = $_POST['year'];	
+	$tel = $_POST['tel'];
+	$radiobutton = $_POST['radio'];
 	$message = $_POST['message'];
+	$pol;
+	if ($radiobutton=='m')
+		{echo $pol="мужской";}
+	else echo $pol="женский";
 	echo 'Имя: ' . $name . '<br/>';
 	echo 'Сообщение: ' . $message . '<br/>';
-	print mail("limansa@yandex.ru","header", "$name1\n $message1", 'From: limansa1997@gmail.com');
+	echo 'пол ' . $pol . '<br>';
+	print mail("limansa@yandex.ru","Анкета", "$name\n $surname\n $date $month $year\n $tel\n $pol\n $message", 'From: limansa1997@gmail.com');
 ?>
